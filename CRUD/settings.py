1830d6722da83443b8f6cdd0e47e945b0d0f5619
sher95 <sher95c@gmail.com>
2021-06-20 19:00:11 +0500

    done

--- CRUD/settings.py
+++ CRUD/settings.py
@@ -39,15 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'register',
     'crispy_forms',
-    'django_jenkins'
-]
-
-JENKINS_TASKS = [
-    'django_jenkins.tasks.run_pep8',
-    'django_jenkins.tasks.run_pyflakes',
-    'django_jenkins.tasks.run_jslint',
-    'django_jenkins.tasks.run_csslint',
-    'django_jenkins.tasks.run_sloccount'
 ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
